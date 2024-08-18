447d9e92b09918b4be897c83db95efde6db7301e
nikolay <nazarov.nikolay15@wb.ru>
2024-08-18 18:39:07 +0300

    1

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Пример данных
+data = [
+    {"id": 1, "name": "Item 1"},
+    {"id": 2, "name": "Item 2"},
+]
+
+# Главная страница
+@app.route('/')
+def index():
+    return jsonify({"message": "Welcome to the API!"})
+
+# Получение всех элементов
+@app.route('/items', methods=['GET'])
+def get_items():
+    return jsonify(data)
+
+# Получение элемента по ID
+@app.route('/items/<int:item_id>', methods=['GET'])
+def get_item(item_id):
+    item = next((item for item in data if item["id"] == item_id), None)
+    return jsonify(item) if item else jsonify({"error": "Item not found"}), 404
+
+# Добавление нового элемента
+@app.route('/items', methods=['POST'])
+def add_item():
+    new_item = request.json
+    new_item['id'] = len(data) + 1
+    data.append(new_item)
+    return jsonify(new_item), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
