e1cc563900253e3f5eea367454274863e671f157
nikolay <nazarov.nikolay15@wb.ru>
2024-08-24 17:27:25 +0300

    4

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+
 from models import db, bcrypt
 from routes import routes
 from config import Config
